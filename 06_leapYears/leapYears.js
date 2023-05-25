906ea970d28516262bb9a241c0169a84619e03f6
ericaofthewired <ericaofthewired@proton.me>
2023-05-25 20:28:27 +0100

    completed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // checks if year is divisible by 4 and not by 100, and returns true if so.
+    if (year % 4 === 0 && year % 100 !== 0) return true;
+    // checks if year is divisible by 100 and 400, and return true if so.
+    else if (year % 100 === 0 && year % 400 === 0) return true;
+    // returns false if neither condition was met
+    else return false;
 };
 
 // Do not edit below this line
