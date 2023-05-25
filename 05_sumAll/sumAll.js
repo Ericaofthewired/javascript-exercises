a9fdb126addc0ea0f6babf57ea39b7b3b264fd09
ericaofthewired <ericaofthewired@proton.me>
2023-05-25 19:34:37 +0100

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (num1 < num2) {
+        if (num1 < 0 || num2 < 0) return "ERROR";
+        if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+        for (let i = num1; i <= num2; i++) {
+            finalSum = finalSum + i;
+        }
+        return finalSum;
+    }
+    else {
+        if (num1 < 0 || num2 < 0) return `ERROR`;
+        if (!Number.isInteger(num1) || !Number.isInteger(num2)) return `ERROR`; 
+        for (let i = num2; i <= num1; i++) {
+            finalSum = finalSum + i;
+        }
+        return finalSum;
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
