f5f8eae60eab1cc5d714f5fa65d809e31690218e
ericaofthewired <ericaofthewired@proton.me>
2023-05-25 18:16:31 +0100

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function (array, ...args) {
+    const fixedArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        fixedArray.push(item);
+      }
+    });
+    return fixedArray;
+  };
 // Do not edit below this line
 module.exports = removeFromArray;
