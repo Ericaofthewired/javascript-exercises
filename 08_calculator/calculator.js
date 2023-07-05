8be8d46282ea42e152a03f2e01c7429e24b4ebe8
ericaofthewired <ericatevissen@proton.me>
2023-07-05 17:55:45 +0100

    completed multiply and power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,25 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length 
+    ? array.reduce((total, num) => total * num)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let powered = num1;
+	for (let i = 1; i < num2; i++) powered = powered * num1;
+  return powered;
 };
 
 const factorial = function() {
