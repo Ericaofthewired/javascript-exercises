3913b622520c854e24a2dab0bc9d062bca85bd64
ericaofthewired <ericatevissen@proton.me>
2023-07-05 21:32:32 +0100

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let num1 = 0
+    let num2 = 1
+    let nextTerm;
+    if (input < 0) return "OOPS";
+    for (let i = 1; i < input; i++) {
+       nextTerm = num1 + num2
+       num1 = num2
+       num2 = nextTerm
+    }
+    return num2
 };
 
 // Do not edit below this line
